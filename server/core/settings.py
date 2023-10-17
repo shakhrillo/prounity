8f85d95e2656a55eb624817066a0a6f9c26f0957
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2023-10-17 12:10:41 +0300

    Django packaging

--- server/core/settings.py
+++ server/core/settings.py
@@ -71,18 +71,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-   "default": {
-      "ENGINE": "djongo",
-      "NAME": "ibro",
-   }
-
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
