2174ce791619ef1af06435741722c5d6e1846d3e
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2023-10-17 11:42:40 +0300

    Authe Create

--- server/core/settings.py
+++ server/core/settings.py
@@ -71,13 +71,19 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+   "default": {
+      "ENGINE": "djongo",
+      "NAME": "ibro",
+   }
 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
