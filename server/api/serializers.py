dde68b50bbf273064f9a510ab54414a4f224bbf2
BobojonovSobir0576 <dweb0987654321@gmail.com>
2023-10-17 19:02:28 +0500

    pylint 7.26

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.models import (User)
-from django.contrib.auth.password_validation import (validate_password)
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 import home.models as mod
-from rest_framework import (serializers)
+from rest_framework import serializers
 
 
 class UserInformationSerializers(serializers.ModelSerializer):
--- server/api/urls.py
+++ server/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import (path)
+from django.urls import path
 import api.views as views
 
 urlpatterns = [
