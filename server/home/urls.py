c59aeccf8b222427309909017fbb6d112dccfebe
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2023-10-17 20:12:40 +0300

    Django pylint

--- server/home/urls.py
+++ server/home/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
-from home.views import *
+from home.views import (
+    sigin_in,
+    sigin_up,
+    logout_user,
+    home,
+    create_product,
+    delete_product,
+    update_product,
+)
 
 urlpatterns = [
-    path('',sigin_in,name='sigin_in'),
-    path('sigin_up/',sigin_up,name='sigin_up'),
-    path('logout_user/',logout_user,name='logout_user'),
-    path('home/',home,name='home'),
-    path('create_product/',create_product,name='create_product'),
-    path('delete_product/<int:pk>/',delete_product,name='delete_product'),
-    path('update_product/<int:pk>/',update_product,name='update_product'),
+    path("", sigin_in, name="sigin_in"),
+    path("sigin_up/", sigin_up, name="sigin_up"),
+    path("logout_user/", logout_user, name="logout_user"),
+    path("home/", home, name="home"),
+    path("create_product/", create_product, name="create_product"),
+    path("delete_product/<int:Id>/", delete_product, name="delete_product"),
+    path("update_product/<int:pk>/", update_product, name="update_product"),
 ]
