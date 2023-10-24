2966cd7b96b8046479bf47df77e568e8e7492d05
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2023-10-24 09:02:16 +0500

    Updated changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
+const path = require('path')
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8000,
+    hot: true
+  },
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
 })
