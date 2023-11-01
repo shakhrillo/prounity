d5b4f2457a9bef2ec4c94a275995ea017f23e493
Mukhammad <muxammadhotamo@gmail.com>
2023-11-01 11:39:55 +0500

    new

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,12 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import Login from "./pages/login";
 import Sidebar from "./pages/sidebar/Sidebar";
+import Navbar from "./pages/Navbar/Navbar";
+import Table from "./pages/Table/Table";
 
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar />
+    <div className="App d-flex">
+        <Sidebar />
+      <div className="w-100">
+        <Navbar/>
+        <Table />
+      </div>
     </div>
   );
 }
