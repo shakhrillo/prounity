10ee6c8e59092c1f0be0bdf65b85f1eefc82c43f
amirbek <amirbekazimov7@gmail.com>
2023-11-02 18:41:59 +0500

    amir

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -8,11 +8,9 @@ import Doctors from "./pages/Doctors/Doctors";
 import Clients from "./pages/Clients/Clients";
 import Category from "./pages/Category/category";
 import EditCategory from "./pages/Category/Edit-category";
-import Patients from './pages/Patients/Patients'
-import PatientDetail from "./pages/Patients/PatientDetail";
-import PatientHistory from "./pages/Patients/PatientHistory";
-import CardHistory from "./pages/Card/Card-history";
+
 import Login from './pages/login'
+import News from "./pages/News/News";
 
 function App() {
   return (
@@ -20,6 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Table />} />
         <Route path="/doctors" element={<Doctors />} />
+        <Route path="/blogs" element={<News />} />
         <Route path="/login" element={<Login />} />
         <Route path="/clients" element={<Clients />} />
         <Route path="/category" element={<Category />} />
