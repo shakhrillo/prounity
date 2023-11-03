6ea7fda479c49e64ae3465f06b2db1d5aef12114
amirbek <amirbekazimov7@gmail.com>
2023-11-03 18:14:31 +0500

    dashboard update

--- dashboard/src/Layout/Layout.jsx
+++ dashboard/src/Layout/Layout.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Sidebar from "../pages/sidebar/Sidebar";
-import Navbar from "../pages/Navbar/Navbar";
 
 const Layout = ({ children }) => {
   return (
     <div className="w-100 d-flex h-100">
-      <Sidebar />
       <div className="w-100 ">
         {children}
       </div>
