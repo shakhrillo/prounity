a629fa78d3e146cca684f09ba9e65e5694effae4
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2023-11-04 15:08:37 +0500

    C

--- js/index.umd.js
+++
@@ -1,5 +0,0 @@
-import Button from './src/button.js';
-
-export default {
-  Button,
-};
--- server/home/urls.py
+++
@@ -1,7 +0,0 @@
-""" Django urls settings """
-from django.urls import path
-from home.views import recaptcha_view
-
-urlpatterns = [
-    path("recaptcha_view/", recaptcha_view, name="recaptcha_view"),
-]
