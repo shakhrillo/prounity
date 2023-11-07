f6c6fe151a763244c77c2d8993c18009ab8da1d2
amirbek <amirbekazimov7@gmail.com>
2023-11-07 12:23:35 +0500

    pu-icon pu-img pu-loading

--- packages/stencil-library/src/components/pu-loading/pu-loading.tsx
+++ packages/stencil-library/src/components/pu-loading/pu-loading.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, Prop, State, Watch, h } from '@stencil/core';
 
 @Component({
   tag: 'pu-loading',
@@ -6,13 +6,42 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class PuLoading {
+  @Prop() duration: number = 2000;
+  @Prop() message: string = '';
+  @State() isLoading = false;
+
+  handleClick() {
+    this.isLoading = true;
+    setTimeout(() => {
+      this.isLoading = false;
+    }, this.duration);
+  }
+
+  @Watch('duration')
+  validateTimeoutSeconds(newValue: number) {
+    if (newValue < 0) {
+      console.warn('Timeout value must be a positive number');
+      this.duration = 2000;
+    }
+  }
 
   render() {
     return (
       <Host>
+        <div>
+          {this.isLoading ? (
+            <div class="loading-overlay">
+              <div class="loading-card">
+                <div class="loading-spinner"></div>
+                {this.message && <div class="message">{this.message}</div>}
+              </div>
+            </div>
+          ) : (
+            <button onClick={() => this.handleClick()}>Load</button>
+          )}
+        </div>
         <slot></slot>
       </Host>
     );
   }
-
 }
